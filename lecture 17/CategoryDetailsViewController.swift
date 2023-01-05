e5f28993bc521c6f1bfd0bb9143326d2cbe2781e
kurdzu <o.katamadze@crystal.ge>
2023-01-05 13:11:43 +0400

    first commit

---
+++ lecture 17/CategoryDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryDetailsViewController.swift
+//  lecture 17
+//
+//  Created by omari katamadze on 05.01.23.
+//
+
+import UIKit
+
+class CategoryDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ lecture 17/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  lecture 17
+//
+//  Created by omari katamadze on 03.01.23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
